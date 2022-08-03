7fc287f7fae22b318e908d98736b0947469c5f59
Chibundo Ilonze <chibundoilonze@gmail.com>
2022-08-03 14:42:29 +0100

    initial commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Admin Panel - Login</title>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="{{ URL::to('img/icon.png') }}">
--- routes/api.php
+++ routes/api.php
@@ -19,27 +19,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['middleware' => ['web']], function () {
-    Route::group(['namespace' => 'Admin'], function(){
-		//Admin Auth Routes
-		Route::get('admin-login', 'Auth\LoginController@showLoginForm')->name('admin.login');
-		Route::post('admin-login', 'Auth\LoginController@login');
-		Route::post('admin-logout', 'Auth\LoginController@logout')->name('admin-logout');
+	Auth::routes();
 
-		Route::middleware(['auth:admin'])->group(function () {
-			Route::resource('admin/role', 'RoleController');
-			Route::resource('admin/permission', 'PermissionController');
-			Route::resource('admin/user', 'UserController');
-			Route::resource('admin/user-update', 'UserUpdateController');	
-		
-			Route::get('/admin/home', 'AdminController@index')->name('admin.home');
-			Route::get('/admin', 'AdminController@admin');
-			Route::get('/', 'AdminController@admin');
-		
-			Route::get('calender', 'CalendarController@index')->name('calender');
-			Route::post('calender/create', 'CalendarController@create')->name('calender.create');
-			Route::post('calender/update', 'CalendarController@update')->name('calender.update');
-			Route::post('calender/delete', 'CalendarController@delete')->name('calender.delete');
-		});
+	Route::middleware(['auth'])->group(function () {
+		Route::get('activities', 'CalendarController@activity')->name('activity.index');
+		Route::get('find-activity', 'CalendarController@findActivity')->name('activity.find');
 	});
 	
 });
